8761ac35d2bd9ad74eed175b08eab7cdf0c8ea84
Wouter Geraedts <git@woutergeraedts.nl>
2024-02-19 15:05:22 +0100

    WIP partitions and LED controller

--- fw/main/led.c
+++ fw/main/led.c
@@ -1,78 +1,61 @@
 #include "led.h"
 
-#include <driver/ledc.h>
+#include <driver/rmt_tx.h>
 #include <esp_log.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/semphr.h>
 
+#include "util/led_strip_encoder.h"
+
 #define TAG "led"
 
-#define LEDC_TIMER LEDC_TIMER_1
-#define LEDC_MODE LEDC_LOW_SPEED_MODE
-#define LEDC_DUTY_RES LEDC_TIMER_8_BIT
-#define LEDC_FREQUENCY (100000)
-
-#define LEDC_R_CHANNEL (LEDC_CHANNEL_5)
-#define LEDC_G_CHANNEL (LEDC_CHANNEL_6)
-#define LEDC_B_CHANNEL (LEDC_CHANNEL_7)
-
-#define GPIO_R_PWM (48)
-#define GPIO_G_PWM (47)
-#define GPIO_B_PWM (35)
+#define RMT_LED_STRIP_RESOLUTION_HZ 10000000 // 10MHz resolution, 1 tick = 0.1us (led strip needs a high resolution)
+#define RMT_LED_STRIP_GPIO_NUM 0
+#define LED_NUMBERS 10
 
 static SemaphoreHandle_t s_mutex;
+static uint8_t led_strip_pixels[LED_NUMBERS * 3];
 
-// static esp_err_t channel_config(ledc_channel_t channel, int gpio_num)
-// {
-//     ledc_channel_config_t config = {
-//         .speed_mode = LEDC_MODE,
-//         .channel = channel,
-//         .timer_sel = LEDC_TIMER,
-//         .intr_type = LEDC_INTR_DISABLE,
-//         .gpio_num = gpio_num,
-//         .duty = 0xff, // Set duty to 100% (inverted)
-//         .hpoint = 0
-//     };
-//     return ledc_channel_config(&config);
-// }
-
-// static bool channel_persist_channel_unsafe(ledc_channel_t channel, uint8_t duty)
-// {
-//     if (duty == 0x00) {
-//         ESP_ERROR_CHECK(ledc_stop(LEDC_MODE, channel, 1)); // Inverted
-//     } else {
-//         ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, channel, 0xff - duty)); // Inverted
-//         ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, channel));
-//     }
-
-//     return duty > 0;
-// }
+static rmt_channel_handle_t m_led_chan = NULL;
+static rmt_encoder_handle_t m_led_encoder = NULL;
 
 esp_err_t led_init(void)
 {
     s_mutex = xSemaphoreCreateMutex();
 
-    // ledc_timer_config_t ledc_timer = {
-    //     .speed_mode = LEDC_MODE,
-    //     .timer_num = LEDC_TIMER,
-    //     .duty_resolution = LEDC_DUTY_RES,
-    //     .freq_hz = LEDC_FREQUENCY,
-    //     .clk_cfg = LEDC_AUTO_CLK
-    // };
-    // ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+    rmt_tx_channel_config_t tx_chan_config = {
+        .clk_src = RMT_CLK_SRC_DEFAULT, // select source clock
+        .gpio_num = RMT_LED_STRIP_GPIO_NUM,
+        .mem_block_symbols = 64, // increase the block size can make the LED less flickering
+        .resolution_hz = RMT_LED_STRIP_RESOLUTION_HZ,
+        .trans_queue_depth = 4, // set the number of transactions that can be pending in the background
+    };
+    ESP_ERROR_CHECK(rmt_new_tx_channel(&tx_chan_config, &m_led_chan));
 
-    // ESP_ERROR_CHECK(channel_config(LEDC_R_CHANNEL, GPIO_R_PWM));
-    // ESP_ERROR_CHECK(channel_config(LEDC_G_CHANNEL, GPIO_G_PWM));
-    // ESP_ERROR_CHECK(channel_config(LEDC_B_CHANNEL, GPIO_B_PWM));
+    led_strip_encoder_config_t encoder_config = {
+        .resolution = RMT_LED_STRIP_RESOLUTION_HZ,
+    };
+    ESP_ERROR_CHECK(rmt_new_led_strip_encoder(&encoder_config, &m_led_encoder));
+
+    ESP_ERROR_CHECK(rmt_enable(m_led_chan));
 
     return ESP_OK;
 }
 
 void led_set_color(rgb_t rgb)
 {
-    // xSemaphoreTake(s_mutex, portMAX_DELAY);
-    // channel_persist_channel_unsafe(LEDC_R_CHANNEL, rgb.r);
-    // channel_persist_channel_unsafe(LEDC_G_CHANNEL, rgb.g);
-    // channel_persist_channel_unsafe(LEDC_B_CHANNEL, rgb.b);
-    // xSemaphoreGive(s_mutex);
+    xSemaphoreTake(s_mutex, portMAX_DELAY);
+
+    led_strip_pixels[0] = rgb.r;
+    led_strip_pixels[1] = rgb.g;
+    led_strip_pixels[2] = rgb.b;
+
+    rmt_transmit_config_t tx_config = {
+        .loop_count = 0, // no transfer loop
+    };
+
+    ESP_ERROR_CHECK(rmt_transmit(m_led_chan, m_led_encoder, led_strip_pixels, sizeof(led_strip_pixels), &tx_config));
+    ESP_ERROR_CHECK(rmt_tx_wait_all_done(m_led_chan, portMAX_DELAY));
+
+    xSemaphoreGive(s_mutex);
 }
